61edf8b425f6a1e29175d897e80694e16f44a6cc
Mychel Albuquerque <mychelalbu@gmail.com>
2023-01-30 19:05:57 -0300

    :sparkles: feat: resolves req06.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -27,7 +27,18 @@ const getAllUsers = async (req, res) => {
   return res.status(200).json(allUsers);
 };
 
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findOne({
+    where: { id },
+    attributes: { exclude: ['password'] },
+  });
+  if (!user) return res.status(404).json({ message: 'User does not exist' });
+  return res.status(200).json(user);
+};
+
 module.exports = {
   addNewUser,
   getAllUsers,
+  getUserById,
 };
--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -7,5 +7,6 @@ const tokenValidation = require('../middlewares/tokenValidation');
 
 router.post('/', userController.addNewUser);
 router.get('/', tokenValidation, userController.getAllUsers);
+router.get('/:id', tokenValidation, userController.getUserById);
 
 module.exports = router;
