da77134c2b3d0837c3f84cf3a2d8e1019f18b977
Mychel Albuquerque <mychelalbu@gmail.com>
2023-01-30 22:06:57 -0300

    :sparkles: feat: resolves req09.

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,4 +1,5 @@
 const categoryService = require('../services/category.service');
+const { Category } = require('../models');
 
 const addCategory = async (req, res) => {
   const { name } = req.body;
@@ -7,6 +8,13 @@ const addCategory = async (req, res) => {
   const newCategory = await categoryService.addCategory(name);
   return res.status(201).json(newCategory);
 };
+
+const getAllcategories = async (req, res) => {
+  const allcategories = await Category.findAll();
+  return res.status(200).json(allcategories);
+};
+
 module.exports = {
   addCategory,
+  getAllcategories,
 };
--- src/routers/category.router.js
+++ src/routers/category.router.js
@@ -6,7 +6,7 @@ const categoryController = require('../controllers/category.controller');
 const tokenValidation = require('../middlewares/tokenValidation');
 
 router.post('/', tokenValidation, categoryController.addCategory);
-// router.get('/', tokenValidation, userController.getAllUsers);
+router.get('/', tokenValidation, categoryController.getAllcategories);
 // router.get('/:id', tokenValidation, userController.getUserById);
 
 module.exports = router;
