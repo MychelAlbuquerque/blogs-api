cceda9d4abbf20f5fbd60835597e35806199d168
Mychel Albuquerque <mychelalbu@gmail.com>
2023-01-31 06:25:41 -0300

    :sparkles: feat: Resolves req 11

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -1,25 +1,28 @@
-const PostCategorySchema = (sequelize, DataTypes) =>{
+const PostCategorySchema = (sequelize, DataTypes) => {
   const PostCategoryTable = sequelize.define("PostCategory",
-    {},
+    {
+      postId: DataTypes.INTEGER,
+      categoryId: DataTypes.INTEGER,
+    },
     {
       timestamps: false,
       tableName: "post_category",
       underscored: true
     });
 
-  PostCategoryTable.associate = ({Category, BlogPost}) =>{
+  PostCategoryTable.associate = ({ Category, BlogPost }) => {
     BlogPost.belongsToMany(Category, {
       as: 'categories',
-      
-      foreignKey:'post_id',
-      otherKey:'category_id',
+
+      foreignKey: 'post_id',
+      otherKey: 'category_id',
       through: PostCategoryTable
     })
 
     Category.belongsToMany(BlogPost, {
       as: 'posts',
-      foreignKey:'category_id',
-      otherKey:'post_id',
+      foreignKey: 'category_id',
+      otherKey: 'post_id',
       through: PostCategoryTable
     })
   }
